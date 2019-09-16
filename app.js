0ab37a650248ff3876c657e05da0b347f591b06e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-09-16 20:21:32 +0200

    add npm packages

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const asyncHandler = require('express-async-handler');
+const googleTrends = require('google-trends-api');
+const https = require('https');
+const app = express();
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// cors
+app.use(cors());
+
+app.get('/google', asyncHandler(async (req, res, next) => {
+    try {
+        googleTrends.dailyTrends({ geo: req.query.country })
+            .then(function (results) {
+                const finalRes = JSON.parse(results)
+                res.status(200).json(finalRes);
+                next();
+            })
+            .catch(function (err) {
+                next(err)
+            });
+    } catch (error) {
+        next(error);
+    }
+}));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
